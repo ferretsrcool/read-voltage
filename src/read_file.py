770b033efbc0453c6ff83756eadd0de0db4a3005
geofm61 <geo.f.m.61@gmail.com>
2019-02-21 17:26:46 +0000

    ADC conversin

--- src/read_file.py
+++ src/read_file.py
@@ -1,41 +1,38 @@
-#import smbus
-import time
 
-#adc_add = tbd
-#reg = tbd
-
-v_ref = 2.048 #tbd
-lsb = 2 * v_ref / (1 << 16) #size of quantisation level
-pga = 1 #programmable gain amplifier of adc
-
-gain = 1 #tbd based on amplifier
-
-#reads the i2c input data and returns the adc output code
+# reads the i2c input data and returns the adc output code
 def read_data():
-    #data_read = bus.read_i2c_block_data(adc_add, reg, 2) # (i2c address, register to be stored in, number of bytes to be read)
-    data_read = [0x00, 0xFF] #format of the data read above [msbyte, lsbyte]
 
-    temp = (data_read[0] << 8) | data_read[1] #concatinates the bytes of the input data
+    # adc_add = 0x60 for example, address of the adc on i2c bus
+    # reg = 0x01 address for the data from bus to be saved to
+    # data_read = bus.read_i2c_block_data(adc_add, reg, 2)
+    #             (i2c address, register to be stored in, number of bytes to be read)
+
+    data_read = [0x00, 0xFF] # format of the data read above [msbyte, lsbyte], temporary
+
+    temp = (data_read[0] << 8) | data_read[1] # concatenates the bytes of the input data
 
     val = twos_comp(temp) # converts twos compliment number
 
     return val
 
-#calculates the input voltage to the adc from the received code
+# calculates the input voltage to the adc from the received code
 def conv_voltage(val):
-    global lsb, pga
+
+    v_ref = 2.048  # full range of input voltage
+    lsb = 2 * v_ref / (1 << 16)  # size of quantisation level
+    pga = 1  # programmable gain amplifier of adc
 
     voltage = val*lsb / pga
 
     return voltage
 
-#calculates the magnetic field from a recorded voltage
+# calculates the magnetic field from a recorded voltage
 def conv_magnetic(voltage):
-    global gain
+    gain = 1 # tbd based on amplifier circuit
 
     voltage = voltage/gain
 
-    #tbd conversion of magnetic field to voltage based on probe
+    # tbd conversion of magnetic field to voltage based on probe
 
     return voltage
 
@@ -45,34 +42,11 @@ def twos_comp(val):
         val = val - (1 << 16)
     return val
 
-def av_list(list):
-    l = len(list)
-    tot = 0
-    for i in range (0, l):
-        tot = tot + list[i]
-    av = tot/l
-    return av
 
-time.sleep(1) # setup time
-
-#getting the first 10 values
-val = read_data()
-voltage = conv_voltage(val)
-mag = conv_magnetic(voltage)
-mag_list = [mag]
-for i in range (0,9):
+def get_sample():
     val = read_data()
     voltage = conv_voltage(val)
     mag = conv_magnetic(voltage)
-    mag_list.append(mag)
-    time.sleep(0.1)
+    return mag
 
-while(1):
-    val = read_data()
-    voltage = conv_voltage(val)
-    mag = conv_magnetic(voltage)
-    mag_list.append(mag)
-    del mag_list[0]
-    averageM = av_list(mag_list)
-    print(averageM)
-    time.sleep(0.1)
+
